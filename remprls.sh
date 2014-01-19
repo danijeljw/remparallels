56f2974392ad7dc8b1ce797d3c9ec38f8d7abc18
Danijel J <danijel.james@gmail.com>
2014-01-20 00:00:50 +1100

    vers 2.1.3 >> 3.0.3

--- remprls.sh
+++ remprls.sh
@@ -1,55 +1,55 @@
-#!/bin/bash
+#!/bin/sh
 
-version="2.1.3"
+# Copyright (c) 2013-2014, Danijel James.
 
-###############################################################################
-# Load All Functions                                                          #
-###############################################################################
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 
-# Function Show script help
-function showHelp {
-        clear
-        echo "Remove Parallels $version"
-        echo 
-        echo 'Usage: $0 [option]'
-        echo 
-        echo 'Option:'
-        echo '  -h | --help            This help file'
-        echo '  -r | --remove          Remove Parallels completely'
-        echo '  -s | --save-license    Remove Parallels, save License'
-        echo 
-}   # end of showHelp
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 
-# request `sudo` access
-function reqSudo {
-sudo -v
-}   # end of reqSudo
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-# keep-alive
-function keepAlive {
-while true; do sudo -n true; sleep 60; kill -0 '$$' || exit; done 2>/dev/null &
-}   # end of keepAlive
+program=`basename $0`
 
-# stop parallels service
-function stopPrls {
-for pid in $(ps aux | grep "Parallels Desktop.app" | awk '{print $2}'); do echo kill -KILL $pid; done
+version="3.0.3"
+
+if [ $# = 0 ]
+then
+    echo "usage: $program [option]"
+    echo ""
+    echo "$program Version $version"
+    echo ""
+    echo "Option:"
+    echo "   -r   Remove Parallels"
+    echo "   -s   Save License and Remove"
+    exit 1
+fi
+
+
+# Stop Parallels if running and unload Kernel Extensions
+stopPrls() {
+echo "Stopping Parallels"
+sleep 5
+for pid in $(ps aux | grep "Parallels*" | awk '{print $2}'); do kill -HUP $pid; done
+echo "Unloading Kernel Extensions"
+sleep 5
 for kext in $(kextstat | grep parallels | awk '{print $6}'); do kextunload $kext; done
-}   # end of stopPrls
-
-# remove XML License
-function remLicense {
-sudo rm /Library/Preferences/Parallels/licenses.xml
-}   # end of remLicense
-
-# save XML License
-function saveLicense {
-mkdir "~/Desktop/Saved\ Parallels\ License"
-mv /Library/Preferences/Parallels/licenses.xml ~/Desktop/Saved\ Parallels\ Licence/licenses.xml
-curl https://raw.github.com/danijeljames/remparallels/master/Removed-License.txt -o ~/Desktop/Saved\ Parallels\ Licence/Removed-License.txt
-}   # end of saveLicense
+}
 
 # remove User Library data
-function remULibrary {
+remULibrary() {
+echo "Removing User Library Data"
+sleep 5
 sudo rm -rf ~/Library/Preferences/com.parallels.*
 sudo rm -rf ~/Library/Preferences/Parallels/*
 sudo rm -rf ~/Library/Preferences/Parallels*
@@ -63,7 +63,9 @@ sudo rm -rf ~/Library/Saved\ Application\ State/com.parallels.*
 }
 
 # remove System Library Data
-function remSLibrary {
+remSLibrary() {
+echo "Removing System Library Data"
+sleep 5
 sudo rm -rf /Library/Logs/parallels*
 sudo rm -rf /Library/Logs/Parallels*
 sudo rm -rf /Library/logs/parallels.log
@@ -73,7 +75,9 @@ sudo rm -rf /Library/Preferences/Parallels
 }
 
 # remove Core Application Data
-function remCoreData {
+remCoreData() {
+echo "Removing Core Application Data"
+sleep 5
 sudo rm -rf /private/var/db/parallels/stats/*
 sudo rm -rf /private/var/db/Parallels/stats/*
 sudo rm -rf /private/var/db/parallels/stats
@@ -89,49 +93,49 @@ sudo rm -rf /private/tmp/com.apple.installer*
 sudo rm /System/Library/Extensions/prl*
 }
 
-# clean up
-function cleanUp {
-sudo periodic daily weekly monthly
+advRestart() {
+echo "It is advised you restart your system"
+echo "to complete the removal process..."
+sleep 5
 }
 
-# Function call -r
-function callR {
-reqSudo
-keepAlive
-stopPrls
-remLicense
-remULibrary
-remSLibrary
-remCoreData
-cleanUp
-}   # end of callR
-
-# Function call -s
-function callS {
-reqSudo
-keepAlive
-stopPrls
-saveLicense
-remULibrary
-remSLibrary
-remCoreData
-cleanUp
-}   # end of callS
-
-###############################################################################
-# Standard UI                                                                 #
-###############################################################################
-
-case $1 in
-        -h | --help )           showHelp
-                                exit
-                                ;;
-        -r | --remove )         callR
-                                exit
-                                ;;
-        -s | --save-license )   callS   
-                                exit
-                                ;;
-        * )                     showHelp
-                                exit 1
-esac
+args=
+for arg in $*; do
+    case $arg in
+	-r)
+		sudo -v
+		stopPrls
+		sudo rm /Library/Preferences/Parallels/licenses.xml
+	    remULibrary
+		remSLibrary
+		remCoreData
+		advRestart
+	    exit 0
+	    ;;
+	-s)
+		sudo -v
+		stopPrls
+		if [ -f /Library/Preferences/Parallels/licenses.xml ]; then
+			echo "Saving Parallels License to Desktop"
+			sleep 5
+			mkdir -p ~/Desktop/SavedPrlsLicense
+			/usr/bin/printf "The License for Parallels is called license.xml\n\nThis file has been saved to this directory. You will be required\nto replace this file if you install Parallels onto a new system,\nor this system again.\n\nPlease consult with Parallels for further information." >> ~/Desktop/SavedPrlsLicense/ReadMe.txt
+			sudo cp /Library/Preferences/Parallels/licenses.xml ~/Desktop/SavedPrlsLicense/
+			sudo rm -f /Library/Preferences/Parallels/licenses.xml
+		fi
+		remULibrary
+		remSLibrary
+		remCoreData
+		advRestart
+		exit 0
+		;;
+	--help|-h)
+        showHelp
+	    exit 0
+	    ;;
+	-*)
+	    echo "$program: $arg: unknown option"
+	    exit 1
+	    ;;
+    esac
+done
